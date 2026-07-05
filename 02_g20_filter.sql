c728dee2e754eca9cd17387b302e06367ccf4220
zayenafarin <zayenafarin@gmail.com>
2026-07-05 14:13:44 +1000

    Update 02_g20_filter.sql

--- 02_g20_filter.sql
+++ 02_g20_filter.sql
@@ -1,1 +1,13 @@
-
+SELECT country, year, co2, gdp, co2_per_capita,
+       co2_per_gdp, coal_co2, oil_co2, gas_co2, population
+FROM emissions
+WHERE country IN ('Australia', 'Brazil', 'Canada', 'China',
+  'France', 'Germany', 'India', 'Indonesia',
+  'Italy', 'Japan', 'Mexico', 'Russia',
+  'Saudi Arabia', 'South Africa', 'South Korea',
+  'Turkey', 'United Kingdom', 'United States'
+)
+AND year BETWEEN 2000 AND 2022
+AND co2 IS NOT NULL 
+AND gdp IS NOT NULL 
+ORDER BY country, year; 
